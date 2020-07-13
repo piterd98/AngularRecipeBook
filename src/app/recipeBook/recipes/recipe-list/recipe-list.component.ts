ae261465cb1a787bc587ea189ffa586846a73b08
Piotr Dajewski <piterd1998@o2.pl>
2020-07-13 18:25:20 +0200

    dynamical recipes

--- src/app/recipeBook/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipeBook/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+ recipes:Recipe[] = [
+   new Recipe('First Recipe','Test recipe','https://c.pxhere.com/photos/0e/3a/oysters_prawns_clams_crab_fish_restaurant_seafood_ocean-1376447.jpg!d')
+ ];
   constructor() { }
 
   ngOnInit(): void {
